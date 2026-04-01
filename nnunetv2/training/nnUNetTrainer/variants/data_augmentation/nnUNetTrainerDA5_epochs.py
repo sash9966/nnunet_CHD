75437d75cf36538e0cd664a9b5236027b1f480a7
sash9966 <71472959+sash9966@users.noreply.github.com>
2026-04-01 11:52:17 -0700

    Add nnUNetTrainerDA5_200epochs

--- nnunetv2/training/nnUNetTrainer/variants/data_augmentation/nnUNetTrainerDA5_epochs.py
+++ nnunetv2/training/nnUNetTrainer/variants/data_augmentation/nnUNetTrainerDA5_epochs.py
@@ -14,3 +14,10 @@ class nnUNetTrainerDA5_100epochs(nnUNetTrainerDA5):
                  device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, device)
         self.num_epochs = 100
+
+
+class nnUNetTrainerDA5_200epochs(nnUNetTrainerDA5):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.num_epochs = 200
